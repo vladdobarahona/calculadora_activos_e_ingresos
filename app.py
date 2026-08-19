88cf887d8a12d75e43694e39b5fe17d08c31abb1
Vladimir Alonso Barahona <details_quiconque.7j@icloud.com>
2026-08-19 11:39:27 -0500

    Set Streamlit page layout to wide

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import re
 import streamlit as st
 import pandas as pd 
-
+st.set_page_config(layout="wide")
 # ==============================
 # Estilos personalizados
 # ==============================
